51172dfc9b1644ae31d8a8dec61a55206a1097f0
Manupati Ramana Kumar <manupatiramanakumar106@gmail.com>
2025-10-15 21:46:43 +0530

    updated layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Providers } from "@/app/components/providers";
-
+import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
